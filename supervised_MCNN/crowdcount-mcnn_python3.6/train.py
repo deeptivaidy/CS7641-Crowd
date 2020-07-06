db418d5c06be3f10ad7138ae37dfca7b46a8d286
Guan <yguan44@gatech.edu>
2020-07-06 11:24:21 -0400

    training curve plot added

--- supervised_MCNN/crowdcount-mcnn_python3.6/train.py
+++ supervised_MCNN/crowdcount-mcnn_python3.6/train.py
@@ -1,19 +1,21 @@
 import os
 import torch
 import numpy as np
+import pickle
+import matplotlib.pyplot as plt
 import sys
 
-from src.crowd_count import CrowdCounter
-from src import network
-from src.data_loader import ImageDataLoader
-from src.timer import Timer
-from src import utils
-from src.evaluate_model import evaluate_model
+from crowd_count import CrowdCounter
+import network
+from data_loader import ImageDataLoader
+from timer import Timer
+import utils
+from evaluate_model import evaluate_model
 
 try:
     from termcolor import cprint
 except ImportError:
-    cprint = None
+    cprint = NoneNo
 
 try:
     from pycrayon import CrayonClient
@@ -27,8 +29,30 @@ def log_print(text, color=None, on_color=None, attrs=None):
     else:
         print(text)
 
+def plot_training_curve():
+    save_name = os.path.join(output_dir, 'training_curves.pkl')
+    f = open(save_name, "rb")
+    data = pickle.load(f)
+    epoch_loss = data[0]
+    epoch_mae = data[1]
+    epoch_mse = data[2]
+    epoch = range(len(epoch_loss))
 
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    ax1.plot(epoch, epoch_loss)
+    ax1.set_title("Training Loss")
+    ax1.set(xlabel="Epoch", ylabel="Loss")
+    ax2.plot(epoch, epoch_mae)
+    ax2.set_title("Mean Absolute Error")
+    ax2.set(xlabel="Epoch", ylabel="MAE")
+    ax3.plot(epoch, epoch_mse)
+    ax3.set_title("Mean Squared Error")
+    ax3.set(xlabel="Epoch", ylabel="MSE")
+    plt.autoscale(enable=True, axis="both")
+    plt.show()
 
+DEBUG = False
+Print_training_curve = False
 method = 'mcnn'
 dataset_name = 'shtechA'
 output_dir = './saved_models/'
@@ -38,9 +62,18 @@ train_gt_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/train_d
 val_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/val'
 val_gt_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/val_den'
 
+if DEBUG:
+    train_path = './data_debug/formatted_trainval/shanghaitech_part_A_patches_9/train'
+    train_gt_path = './data_debug/formatted_trainval/shanghaitech_part_A_patches_9/train_den'
+    val_path = './data_debug/formatted_trainval/shanghaitech_part_A_patches_9/val'
+    val_gt_path = './data_debug/formatted_trainval/shanghaitech_part_A_patches_9/val_den'
+
+if Print_training_curve:
+    plot_training_curve()
+
 #training configuration
 start_step = 0
-end_step = 2000
+end_step = 5
 lr = 0.00001
 momentum = 0.9
 disp_interval = 500
@@ -87,6 +120,9 @@ if use_tensorboard:
 
 # training
 train_loss = 0
+epoch_loss = []
+epoch_mae = []
+epoch_mse = []
 step_cnt = 0
 re_cnt = False
 t = Timer()
@@ -124,24 +160,36 @@ for epoch in range(start_step, end_step+1):
 
             log_print(log_text, color='green', attrs=['bold'])
 
+    # Append train_loss
+    epoch_loss.append(train_loss)
+
+
 
     if (epoch % 10 == 0):
         save_name = os.path.join(output_dir, '{}_{}_{}.h5'.format(method,dataset_name,epoch))
-        network.save_net(save_name, net)     
-        #calculate error on the validation dataset 
-        mae,mse = evaluate_model(save_name, data_loader_val)
+        network.save_net(save_name, net)
+
+        # calculate error on the validation dataset
+        mae, mse = evaluate_model(save_name, data_loader_val)
+        # Append MAE MSE
+        epoch_mae.append(mae)
+        epoch_mse.append(mse)
+        # save trainning curves
+        save_name = os.path.join(output_dir, 'training_curves.pkl')
+        f = open(save_name, "wb")
+        pickle.dump([epoch_loss, epoch_mae, epoch_mse], f)
+        f.close()
+
         if mae < best_mae:
             best_mae = mae
             best_mse = mse
             best_model = '{}_{}_{}.h5'.format(method,dataset_name,epoch)
         log_text = 'EPOCH: %d, MAE: %.1f, MSE: %0.1f' % (epoch,mae,mse)
         log_print(log_text, color='green', attrs=['bold'])
-        log_text = 'BEST MAE: %0.1f, BEST MSE: %0.1f, BEST MODEL: %s' % (best_mae,best_mse, best_model)
+        log_text = 'BEST MAE: %0.1f, BEST MSE: %0.1f, BEST MODEL: %s' % (best_mae, best_mse, best_model)
         log_print(log_text, color='green', attrs=['bold'])
         if use_tensorboard:
             exp.add_scalar_value('MAE', mae, step=epoch)
             exp.add_scalar_value('MSE', mse, step=epoch)
             exp.add_scalar_value('train_loss', train_loss/data_loader.get_num_samples(), step=epoch)
-        
-    
 
